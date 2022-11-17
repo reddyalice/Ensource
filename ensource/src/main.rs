aca21cc6f10928a098b0861a8f382c75f68a6166
Bahar Demircan <kodyenler@hotmail.com>
2022-11-17 11:38:28 +0300

    binary operations

--- ensource/src/main.rs
+++ ensource/src/main.rs
@@ -1,18 +1,9 @@
 extern crate pest;
 #[macro_use]
 extern crate pest_derive;
-
-use pest::Parser;
-use std::fs;
-
-#[derive(Parser)]
-#[grammar = "grammar.pest"]
-pub struct EnsourceParser;
+mod parser;
+use parser::parse;
 
 fn main() {
-    let unparsed = fs::read_to_string("src/test.necr").expect("couldn't read");
-    println!("{:#?}", unparsed);
-    let source = EnsourceParser::parse(Rule::file, &unparsed)
-        .expect("up").next().unwrap();
-    println!("{:#?}", source);
+    parse("src/test.necr");
 }
