329bab4c795bc479bd07b47ce3293948dab2c1e0
Bahar Demircan <kodyenler@hotmail.com>
2022-11-28 01:36:16 +0300

    baby steps

--- ensourcel/src/main.rs
+++ ensourcel/src/main.rs
@@ -1,7 +1,8 @@
+mod ast;
+
 extern crate pest;
 #[macro_use]
 extern crate pest_derive;
-mod ast;
 
 mod parser;
 
--- ensourcel/src/parser.rs
+++ ensourcel/src/parser.rs
@@ -1,5 +1,7 @@
-use pest::Parser;
+
+use pest::{Parser, iterators::Pairs, error::Error};
 use std::fs;
+use crate::ast::*;
 
 #[derive(Parser)]
 #[grammar = "grammar.pest"]
@@ -9,7 +11,22 @@ pub fn parse(filename: &str) {
     let unparsed = fs::read_to_string(filename).expect("Couldn't read");
     println!("{:#?}", unparsed);
     let source = EnsourceLParser::parse(Rule::file, &unparsed);
-    for pair in source {
-        println!("{:#?}", pair);
+    parse_rule(source);
+    
+}
+
+fn parse_rule(rules : Result<Pairs<Rule>, Error<Rule>>){
+    let mut baseCtx : i8;
+
+    for pair in rules.unwrap() {
+        
+        match pair.as_rule() {
+            Rule::ctx=> {
+                
+            },
+
+            
+            _ => ()
+        }
     }
 }
